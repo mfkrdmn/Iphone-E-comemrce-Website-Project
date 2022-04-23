d54a7b21baa541fe56ed646cd52da4a55303b04e
M. Fatih Karaduman <98664346+mfkrdmn@users.noreply.github.com>
2022-04-23 23:40:01 +0300

    Add files via upload

---
+++ 13pro.js
@@ -0,0 +1,129 @@
+
+//videoscrollevent
+
+
+window.addEventListener("scroll",function(){
+
+      
+      if(document.documentElement.scrollTop > 590 && document.documentElement.scrollTop < 1290 ){
+        document.getElementById("sectionthreein").style.opacity=1
+    }
+    else {
+        document.getElementById("sectionthreein").style.opacity=.15
+      }
+
+    if(document.documentElement.scrollTop > 1090 && document.documentElement.scrollTop < 1390 ){
+        document.getElementById("first").style.opacity=1
+    }
+    else {
+        document.getElementById("first").style.opacity=.25
+      }
+      if(document.documentElement.scrollTop > 1390 && document.documentElement.scrollTop < 1590 ){
+        document.getElementById("second").style.opacity=1
+    }
+    else {
+        document.getElementById("second").style.opacity=.25
+      }
+      if(document.documentElement.scrollTop > 1590 && document.documentElement.scrollTop < 1890 ){
+        document.getElementById("third").style.opacity=1
+    }
+    else {
+        document.getElementById("third").style.opacity=.25
+      }
+      if(document.documentElement.scrollTop > 1890 && document.documentElement.scrollTop < 2090 ){
+        document.getElementById("fourt").style.opacity=1
+    }
+    else {
+        document.getElementById("fourt").style.opacity=.25
+      }
+
+    if(document.documentElement.scrollTop > 2090 && document.documentElement.scrollTop < 2290 ){
+        document.getElementById("fift").style.opacity=1
+    }
+    else {
+        document.getElementById("fift").style.opacity=.25
+      }
+
+      if(document.documentElement.scrollTop > 2390 && document.documentElement.scrollTop < 2990 ){
+        document.getElementById("screen2").style.left="1100px"
+    }
+    else {
+      document.getElementById("screen2").style.left="920px"
+    }
+      if(document.documentElement.scrollTop > 2390 && document.documentElement.scrollTop < 2990 ){
+        document.getElementById("screen").style.right="80px"
+    }
+    else {
+      document.getElementById("screen").style.right="0px"}
+
+      if(document.documentElement.scrollTop > 2390 && document.documentElement.scrollTop < 2990 ){
+        document.getElementById("compareh21").style.left="280px"
+    }
+    else {
+      document.getElementById("compareh21").style.left="26%"
+    }
+    
+    if(document.documentElement.scrollTop > 2390 && document.documentElement.scrollTop < 2990 ){
+      document.getElementById("compareh22").style.left="1500px"
+    }
+    else {
+      document.getElementById("compareh22").style.left="63%"
+    }
+
+    if(document.documentElement.scrollTop > 5390 && document.documentElement.scrollTop < 5990 ){
+      document.getElementById("secsixconth11").style.left="950px"
+    }
+    else {
+      document.getElementById("secsixconth11").style.left="750px"
+    }
+    if(document.documentElement.scrollTop > 5390 && document.documentElement.scrollTop < 5990 ){
+      document.getElementById("secsixconth12").style.left="650px"
+    }
+    else {
+      document.getElementById("secsixconth12").style.left="750px"
+    }
+    if(document.documentElement.scrollTop > 5390 && document.documentElement.scrollTop < 5990 ){
+      document.getElementById("secsixconth13").style.left="950px"
+    }
+    else {
+      document.getElementById("secsixconth13").style.left="750px"
+    }
+    if(document.documentElement.scrollTop > 5790 && document.documentElement.scrollTop < 7220 ){
+      document.getElementById("Telephotoh2").style.visibility="visible"
+    }
+    else {
+      document.getElementById("Telephotoh2").style.visibility="hidden"
+    }
+    if(document.documentElement.scrollTop > 5790 && document.documentElement.scrollTop < 7220 ){
+      document.getElementById("line").style.width="270%"
+    }
+    else {
+      document.getElementById("line").style.width="000%"
+    }
+    if(document.documentElement.scrollTop > 6000 && document.documentElement.scrollTop < 7220 ){
+      document.getElementById("line2").style.width="450%"
+    }
+    else {
+      document.getElementById("line2").style.width="000%"
+    }
+    if(document.documentElement.scrollTop > 6200 && document.documentElement.scrollTop < 7220 ){
+      document.getElementById("line3").style.width="385%"
+    }
+    else {
+      document.getElementById("line3").style.width="000%"
+    }
+    if(document.documentElement.scrollTop > 6820 && document.documentElement.scrollTop < 7720 ){
+      document.getElementById("secsevenfirstcont").style.visibility="visible"
+    }
+    else {
+      document.getElementById("secsevenfirstcont").style.visibility="hidden"
+    }
+    if(document.documentElement.scrollTop > 6520 && document.documentElement.scrollTop < 8020 ){
+      document.getElementById("secsevenphotodiv").style.backgroundSize="1400px"
+    }
+    else {
+      document.getElementById("secsevenphotodiv").style.backgroundSize="800px"
+      document.getElementById("secsevenphotodiv").style.backgroundPosition="right"
+    }
+
+})
